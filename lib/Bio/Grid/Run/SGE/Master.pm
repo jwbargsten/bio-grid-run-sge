cb54fcc81edbfcd2ff12950095d6f98903eab115
Joachim Bargsten <jw@bargsten.org>
2017-09-07 00:15:31 +0200

    removed num_parts from master, because not needed anymore, I hope

--- lib/Bio/Grid/Run/SGE/Master.pm
+++ lib/Bio/Grid/Run/SGE/Master.pm
@@ -31,8 +31,6 @@ has 'env'    => ( is => 'rw', required => 1 );
 has 'config' => ( is => 'rw', required => 1 );
 has 'log'    => ( is => 'rw', required => 1 );
 
-has 'num_parts' => ( is => 'rw', default => 0 );
-
 has 'iterator' => ( is => 'rw', lazy_build => 1 );
 
 sub populate {
